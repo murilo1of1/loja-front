04fd1f98996a65cf6311f5de702c97f6f1f31e15
murilo.vidi <murilomendo3@gmail.com>
2025-05-29 18:14:24 -0300

    aa

--- src/components/dialogInfoUser.jsx
+++ src/components/dialogInfoUser.jsx
@@ -30,7 +30,7 @@ export default function DialogInfoUser({ isOpen, onClose, user }) {
   const getInitials = (name) => {
     if (!name) return "";
     const parts = name.trim().split(" ");
-    if (parts.length === 1) return parts[0][0].toUpperCase();
+    if (parts.length === 1) return parts[0][0].toUpperCase(); 
     return (parts[0][0] + parts[1][0]).toUpperCase();
   };
 
